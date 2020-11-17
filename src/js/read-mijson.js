1ccf3b993022932d7f984870a1ebde71f0a36484
colin-combe <colin.combe@ed.ac.uk>
2020-11-17 10:07:30 +0000

    defend against features in complexes that are in participants that aren't found in json (prob becuase its an unexpanded complex, i.e. complex as hexagon symbol)

--- src/js/read-mijson.js
+++ src/js/read-mijson.js
@@ -137,14 +137,18 @@ export function readMijson (/*miJson*/miJson, /*App*/ app, expand = true) {
                 }
                 // console.log("*", mID, seqDatum);
                 const molecule = app.participants.get(mID);
-                const seqFeature = new SequenceDatum(molecule, seqDatum.pos);
-                const annotation = new Annotation(annotName, seqFeature);
-                let miFeatures = molecule.annotationSets.get("MI Features");
-                if (!miFeatures) {
-                    miFeatures = [];
-                    molecule.annotationSets.set("MI Features", miFeatures);
+                if (molecule) {
+                    const seqFeature = new SequenceDatum(molecule, seqDatum.pos);
+                    const annotation = new Annotation(annotName, seqFeature);
+                    let miFeatures = molecule.annotationSets.get("MI Features");
+                    if (!miFeatures) {
+                        miFeatures = [];
+                        molecule.annotationSets.set("MI Features", miFeatures);
+                    }
+                    miFeatures.push(annotation);
+                } else {
+                    console.log("participant " + mID + " not found!");
                 }
-                miFeatures.push(annotation);
             }
         }
     }
